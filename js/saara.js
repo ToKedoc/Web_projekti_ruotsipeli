b90b963be4c5b77d6482c969367765c5dbac673c
SaaraKLaine <saaralaine12@gmail.com>
2024-11-26 20:25:54 +0200

    Some headlines names changed in html-files. In saara.js added two lists, contain finnish and swedish sentences to the game Ord till meningar.

---
+++ js/saara.js
@@ -0,0 +1,17 @@
+// Lista lauseista suomeksi
+const finnSentences = [
+    "Minua kiinnostaa kehittää tulevaisuudessa tekoälyä",
+    "Aloitin juuri uuden kurssin, sillä opiskellaan kyberturvallisuutta",
+    "Pythonin ohjelmointi on minusta helppoa ja hauskaa",
+    "Käytän tietoteknisiä taitoja työssäni",
+    "Tarvitsen työssäni tietokonetta päivittäin"
+]
+
+// Lista Lauseista
+const sentences = [
+    "Jag är intresserad av att utveckla artificiell intelligens i framtiden",
+    "Jag har precis börjat på en ny kurs, eftersom vi studerar cybersäkerhet",
+    "Python-programmering är enkelt och roligt för mig",
+    "Jag använder IT-kunskaper i mitt arbete",
+    "Jag behöver en dator på jobbet varje dag"
+] 
