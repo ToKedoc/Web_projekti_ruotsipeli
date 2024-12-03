0f5bdbdef50efc67bf84e5f2bb1c984707476a09
SaaraKLaine <saaralaine12@gmail.com>
2024-12-03 20:19:16 +0200

    Created function that split sentence, create table and sort words randomly to copy table. Adds words from copy table to wordbox in the site. Created css styles for text and containers for wordbox words and hover.

--- js/saara.js
+++ js/saara.js
@@ -21,5 +21,29 @@ let sentenceIndex = 0
 
 //haetaan HTML-dokumentista elementti id:llä
 const finnBox = document.getElementById("finn-box")
+const sentencebox = document.getElementById("sentence-box")
+const wordbox = document.getElementById("word-box")
+
 //Näytetään suomenkielinen lause sivulla
 finnBox.textContent = finnSentences[sentenceIndex]
+
+//haetaan kierroksessa oleva lause ja sen sisältämät sanat
+//haetaan lause
+const sentence = sentences[sentenceIndex]
+//jaetaan lause sanoiksi taulukkoon
+const words = sentence.split(" ")
+//luodaan taulukko kopio ja sijoitetaan siihen sanat satunnaisessa järjestyksessä
+const mixWords = [...words].sort(() => Math.random() - 0.5)
+
+//lisätään sanat word-boxiin satunnaisessa järjestyksessä
+//käy läpi jokaisen alkion muuttujan mixWords-taulukosta ja tallentaa muuttujaan "word"
+mixWords.forEach(word => {
+    //jokaisella kierroksella luodaan div elementti
+    const wordElement = document.createElement("div")
+    //lisää div elementille luokan, css tyylien luomiseksi
+    wordElement.className = "word"
+    //asettaa kierroksessa olevan word muuttujan arvon elementin arvoksi
+    wordElement.textContent = word
+    //vie sanat paikalleen sanalaatikkoon sivulla
+    wordbox.appendChild(wordElement)
+})
