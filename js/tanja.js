836e571255d4c00d9593d4b7cfd52b9c74dba281
tanjahapponen <happonen.tanja23@gmail.com>
2024-12-10 13:41:12 +0200

    Added questions setup and answer checking. Image, word and answer options are displayed. Game shows a correct or incorrect message after answering and tells when the game is over. Layout of the image, words and buttons updated.

--- js/tanja.js
+++ js/tanja.js
@@ -1,5 +1,5 @@
 const images = [
-    '../images_tanja/img1.png','../images/img2.jpg','../images/images_tanja/img3.png','../images/images_tanja/img4.webp',
+    '../images/images_tanja/img1.png','../images/images_tanja/img2.jpg','../images/images_tanja/img3.png','../images/images_tanja/img4.webp',
     '../images/images_tanja/img5.png','../images/images_tanja/img6.jpg','../images/images_tanja/img7.png','../images/images_tanja/img8.jpg','../images/images_tanja/img9.png','../images/images_tanja/img10.webp'
 ];
 
@@ -25,4 +25,44 @@ const words = [
 const correctAnswers = 
     [0,2,2,0,1,1,0,2,1,0];
 
-let currentIndex = 0;
+let currentIndex = 0; //Aloitta ensimmäisestä kysymyksestä, seuraa mikä kysymys näytetään tällä hetkellä
+
+function setQuestion() {
+    const imageElement = document.querySelector('#picture');
+    const wordElement = document.querySelector('.word');
+    const buttons = document.querySelectorAll('.buttons button');
+
+    // Asettaa kuvan ja sanan
+    imageElement.src = images[currentIndex];
+    wordElement.textContent = words[currentIndex];
+
+    // Asettaa vaihtoehdot
+    buttons.forEach((button, index) => {
+        button.textContent = options[currentIndex][index];
+    });
+}
+
+
+// Tarkistetaan, vastaako käyttäjän valinta oikeaa vastausta (määritelty correctAnswers-listassa).
+
+function checkAnswer(index) {
+    if (index === correctAnswers[currentIndex]) {
+        alert('Ordentligt! :)');
+    } else {
+        alert('Fel! :(');
+    }
+
+    currentIndex++;
+    if (currentIndex < images.length) {
+        setQuestion();
+    } else {
+        alert('Spelet slut!');
+    }
+}
+
+// Käynnistä peli
+document.addEventListener('DOMContentLoaded', () => {
+    setQuestion();
+});
+
+
